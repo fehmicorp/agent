e5846b798424db8c5e049b68e967bfa908904858
m_sam <m_sam@example.com>
2026-06-22 23:27:00 +0530

    Changes to be committed: 	modified:   windows/config/registry/go.mod 	deleted:    windows/config/registry/main.go 	modified:   windows/config/registry/tray.go

--- windows/config/registry/main.go
+++
@@ -1,5 +0,0 @@
-package registry
-
-var (
-	instance *App
-)
--- windows/config/registry/tray.go
+++ windows/config/registry/tray.go
@@ -2,6 +2,10 @@ package registry
 
 import "github.com/fehmicorp/agent/windows/config/types"
 
+var (
+	instance *types.App
+)
+
 func GetTray() *types.Tray {
 
 	if instance == nil {
