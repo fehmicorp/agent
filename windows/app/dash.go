e4874c14b1f06e2dc81affbc13882869bb79a204
Sameer Shaikh <developer@fehmicorp.in>
2026-06-29 18:46:42 +0530

    Changes to be committed: 	modified:   windows/app/dash.go 	deleted:    windows/services/metric/main.go 	modified:   windows/services/metric/start.go

--- windows/app/dash.go
+++ windows/app/dash.go
@@ -9,10 +9,7 @@ import (
 	"unicode"
 
 	"github.com/StackExchange/wmi"
-	"github.com/shirou/gopsutil/v3/cpu"
-	"github.com/shirou/gopsutil/v3/disk"
 	"github.com/shirou/gopsutil/v3/host"
-	"github.com/shirou/gopsutil/v3/mem"
 	"github.com/shirou/gopsutil/v3/net"
 	"github.com/wailsapp/wails/v2/pkg/runtime"
 )
@@ -53,91 +50,91 @@ func (a *App) StartMetric(intervalMs int) {
 	}
 }
 
-func getCPUUsage() string {
-	cpuPercent, err := cpu.Percent(0, false)
-	if err == nil && len(cpuPercent) > 0 {
-		return fmt.Sprintf("%.0f%%", cpuPercent[0])
-	}
+// func getCPUUsage() string {
+// 	cpuPercent, err := cpu.Percent(0, false)
+// 	if err == nil && len(cpuPercent) > 0 {
+// 		return fmt.Sprintf("%.0f%%", cpuPercent[0])
+// 	}
 
-	if err != nil {
-		Logger.Log("ERROR", "CPU metrics collection failed: "+err.Error())
-	}
-	return "0%"
-}
+// 	if err != nil {
+// 		Logger.Log("ERROR", "CPU metrics collection failed: "+err.Error())
+// 	}
+// 	return "0%"
+// }
 
-func getRAMUsage() string {
-	if vm, err := mem.VirtualMemory(); err == nil {
-		return fmt.Sprintf("%.0f%%", vm.UsedPercent)
-	} else {
-		Logger.Log("ERROR", "RAM metrics collection failed: "+err.Error())
-	}
-	return "0%"
-}
+// func getRAMUsage() string {
+// 	if vm, err := mem.VirtualMemory(); err == nil {
+// 		return fmt.Sprintf("%.0f%%", vm.UsedPercent)
+// 	} else {
+// 		Logger.Log("ERROR", "RAM metrics collection failed: "+err.Error())
+// 	}
+// 	return "0%"
+// }
 
-func getDiskUsage(drive string) string {
-	d, err := disk.Usage(drive)
-	if err != nil {
-		Logger.Log("ERROR", "Disk tracking error: "+err.Error())
-		return "0%"
-	}
-	return fmt.Sprintf("%.0f%%", d.UsedPercent)
-}
+// func getDiskUsage(drive string) string {
+// 	d, err := disk.Usage(drive)
+// 	if err != nil {
+// 		Logger.Log("ERROR", "Disk tracking error: "+err.Error())
+// 		return "0%"
+// 	}
+// 	return fmt.Sprintf("%.0f%%", d.UsedPercent)
+// }
 
-func getWmiNetwork() (uint64, uint64, error) {
-	var netAdapters []Win32_PerfFormattedData_Tcpip_NetworkInterface
-	query := "SELECT Name, BytesReceivedPerSec, BytesSentPerSec FROM Win32_PerfFormattedData_Tcpip_NetworkInterface"
-	if err := wmi.Query(query, &netAdapters); err != nil {
-		return 0, 0, err
-	}
-	var rxTotal, txTotal uint64
-	for _, adapter := range netAdapters {
-		name := adapter.Name
-		if contains(name, "Loopback") ||
-			contains(name, "vEthernet") ||
-			contains(name, "Virtual") ||
-			contains(name, "Pseudo") ||
-			contains(name, "Teredo") {
-			continue
-		}
-		rxTotal += adapter.BytesReceivedPerSec
-		txTotal += adapter.BytesSentPerSec
-	}
-	return rxTotal, txTotal, nil
-}
+// func getWmiNetwork() (uint64, uint64, error) {
+// 	var netAdapters []Win32_PerfFormattedData_Tcpip_NetworkInterface
+// 	query := "SELECT Name, BytesReceivedPerSec, BytesSentPerSec FROM Win32_PerfFormattedData_Tcpip_NetworkInterface"
+// 	if err := wmi.Query(query, &netAdapters); err != nil {
+// 		return 0, 0, err
+// 	}
+// 	var rxTotal, txTotal uint64
+// 	for _, adapter := range netAdapters {
+// 		name := adapter.Name
+// 		if contains(name, "Loopback") ||
+// 			contains(name, "vEthernet") ||
+// 			contains(name, "Virtual") ||
+// 			contains(name, "Pseudo") ||
+// 			contains(name, "Teredo") {
+// 			continue
+// 		}
+// 		rxTotal += adapter.BytesReceivedPerSec
+// 		txTotal += adapter.BytesSentPerSec
+// 	}
+// 	return rxTotal, txTotal, nil
+// }
 
-func contains(s, substr string) bool {
-	return strings.Contains(strings.ToLower(s), strings.ToLower(substr))
-}
+// func contains(s, substr string) bool {
+// 	return strings.Contains(strings.ToLower(s), strings.ToLower(substr))
+// }
 
-func formatSpeed(bytes float64) string {
-	const (
-		KB = 1024
-		MB = KB * 1024
-		GB = MB * 1024
-	)
+// func formatSpeed(bytes float64) string {
+// 	const (
+// 		KB = 1024
+// 		MB = KB * 1024
+// 		GB = MB * 1024
+// 	)
 
-	var res string
-	switch {
-	case bytes >= GB:
-		res = fmt.Sprintf("%.2fG", bytes/GB)
-	case bytes >= MB:
-		res = fmt.Sprintf("%.2fM", bytes/MB)
-	case bytes >= KB:
-		res = fmt.Sprintf("%.1fK", bytes/KB)
-	default:
-		return fmt.Sprintf("%.0fB", bytes)
-	}
-	res = strings.TrimSuffix(res, ".00G")
-	res = strings.TrimSuffix(res, ".00M")
-	res = strings.TrimSuffix(res, ".0K")
-	if strings.Contains(res, ".") && (strings.HasSuffix(res, "G") || strings.HasSuffix(res, "M")) {
-		res = strings.TrimSuffix(res, "0G") + "G"
-		res = strings.TrimSuffix(res, "0M") + "M"
-		res = strings.Replace(res, ".G", "G", 1)
-		res = strings.Replace(res, ".M", "M", 1)
-	}
-	return res
-}
+// 	var res string
+// 	switch {
+// 	case bytes >= GB:
+// 		res = fmt.Sprintf("%.2fG", bytes/GB)
+// 	case bytes >= MB:
+// 		res = fmt.Sprintf("%.2fM", bytes/MB)
+// 	case bytes >= KB:
+// 		res = fmt.Sprintf("%.1fK", bytes/KB)
+// 	default:
+// 		return fmt.Sprintf("%.0fB", bytes)
+// 	}
+// 	res = strings.TrimSuffix(res, ".00G")
+// 	res = strings.TrimSuffix(res, ".00M")
+// 	res = strings.TrimSuffix(res, ".0K")
+// 	if strings.Contains(res, ".") && (strings.HasSuffix(res, "G") || strings.HasSuffix(res, "M")) {
+// 		res = strings.TrimSuffix(res, "0G") + "G"
+// 		res = strings.TrimSuffix(res, "0M") + "M"
+// 		res = strings.Replace(res, ".G", "G", 1)
+// 		res = strings.Replace(res, ".M", "M", 1)
+// 	}
+// 	return res
+// }
 
 func getWmiDomain() string {
 	var systemInfo []Win32_ComputerSystem
