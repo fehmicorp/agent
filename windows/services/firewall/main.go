9238e40b80b9ead4ec3623039b1824c0e797629e
Sameer Shaikh <developer@fehmicorp.in>
2026-06-29 18:32:00 +0530

    Changes to be committed: 	modified:   windows/services/firewall/main.go 	modified:   windows/services/firewall/set.go 	modified:   windows/services/firewall/status.go

--- windows/services/firewall/main.go
+++ windows/services/firewall/main.go
@@ -1,24 +1,18 @@
-package main
-
-import (
-	"encoding/json"
-	"fmt"
-	"os"
-)
+package firewall
 
 func main() {
-	result, err := GetFirewallStatus()
-	if err != nil {
-		fmt.Println("Error:", err)
-		os.Exit(1)
-	}
-	out, err := json.MarshalIndent(result, "", "  ")
-	if err != nil {
-		fmt.Println("JSON Error:", err)
-		os.Exit(1)
-	}
+	// result, err := GetFirewallStatus()
+	// if err != nil {
+	// 	fmt.Println("Error:", err)
+	// 	os.Exit(1)
+	// }
+	// out, err := json.MarshalIndent(result, "", "  ")
+	// if err != nil {
+	// 	fmt.Println("JSON Error:", err)
+	// 	os.Exit(1)
+	// }
 
-	fmt.Println(string(out))
+	// fmt.Println(string(out))
 	// fmt.Println("Admin:", IsAdmin())
 
 	// if err := setFirewallDisabled(); err != nil {
