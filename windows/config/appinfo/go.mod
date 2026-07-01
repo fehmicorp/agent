4510bbef6fe4d246afb199cd3e77d6987bc433cc
Sameer Shaikh <developer@fehmicorp.in>
2026-07-01 10:27:43 +0530

    Changes to be committed: 	new file:   windows/config/appinfo/go.mod 	new file:   windows/config/appinfo/main.go 	new file:   windows/services/bitlocker/set.go

---
+++ windows/config/appinfo/go.mod
@@ -0,0 +1,3 @@
+module github.com/fehmicorp/agent/windows/config/appinfo
+
+go 1.26.4
---
+++ windows/config/appinfo/main.go
@@ -0,0 +1,12 @@
+package appinfo
+
+const (
+	Name        = "Fehmi Endpoint Agent"
+	Version     = "v1.0.1"
+	Build       = "20260630"
+	BuildType   = "stable"
+	Company     = "Fehmi Corporation"
+	Website     = "https://fehmicorp.in"
+	Endpoint    = "https://edp.fehmicorp.in"
+	Description = "Unified Endpoint Management Agent"
+)
