c5542150b58e93f7bda711869f17bbf88a7e7e5a
Sameer Shaikh <developer@fehmicorp.in>
2026-07-08 15:55:39 +0530

    Changes to be committed: 	modified:   v1/cmd/go.mod 	new file:   v1/cmd/go.sum 	modified:   v1/cmd/main.go

--- v1/cmd/go.mod
+++ v1/cmd/go.mod
@@ -1,3 +1,5 @@
 module github.com/fehmicorp/agent/v1/cmd
 
 go 1.26.4
+
+require github.com/fehmicorp/agent/v1/app v0.0.0-20260708101016-3fca538fed7a // indirect
--- v1/cmd/main.go
+++ v1/cmd/main.go
@@ -1,14 +1,9 @@
 package main
 
 import (
-	"github.com/fehmicorp/agent/v1/cmd/internal"
+	"github.com/fehmicorp/agent/v1/app"
 )
 
 func main() {
-	println("Build Type: ", internal.Current.BuildType)
-	println("Build Date: ", internal.Current.Build)
-	println("Company: ", internal.Current.Company)
-	println("Version: ", internal.Current.Version)
-
-	// go app.Run()
+	go app.Run()
 }
